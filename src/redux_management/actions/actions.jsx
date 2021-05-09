1759b254321edd02e0a8f00120949b17d783acd8
MANJUNATH KO <37257759+kom3@users.noreply.github.com>
2021-05-09 20:11:11 +0530

    Update actions.jsx

--- src/redux_management/actions/actions.jsx
+++ src/redux_management/actions/actions.jsx
@@ -1,3 +1,10 @@
+//Here 'which_reducer_and_action' parameter is used to detect which reducer? SET_REDUCER_ONE or SET_REDUCER_TWO and
+//which Action? SET or RESET
+//In actions we can receive data in any format we want and can we can parse it accordingly, but it should always return plain json object data having {action:"", payload:""}
+//again these 'action' and 'payload' key names can be anything, we can change it in reducers defination (reducer1.jsx, reducer2.jsx)
+
+
+
 const MyActions = (which_reducer_and_action, what_data) => {
     switch (which_reducer_and_action) {
         case "S_REDUCER_1":
